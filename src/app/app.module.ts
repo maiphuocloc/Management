c533aa3a38afffe5b9d8b2adf64a46a5b3bff64e
maiphuocloc <44157234+maiphuocloc@users.noreply.github.com>
2019-05-14 11:26:16 +0700

    ghi chú

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import  {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './component/signin/signin.component';
 import { HomeComponent } from './component/home/home.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { DetailuserComponent } from './component/detailuser/detailuser.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { DetailuserComponent } from './component/detailuser/detailuser.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
